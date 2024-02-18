2223808f3c39cafda22e51f0a31094e64552d99b
tw4 <88425310+tw4@users.noreply.github.com>
2024-02-18 11:59:49 +0300

    update(core): project name

--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12a4ac7c43b1ed167cd1e9645c184f0308497f7a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da647a394dd957c3a9eee9c3b581a724283c8b84")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
