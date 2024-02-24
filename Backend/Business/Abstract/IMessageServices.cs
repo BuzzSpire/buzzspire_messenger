e5498ac4b17f9cd2d35b3e74daf79eeb5f9fab0d
tw4 <88425310+tw4@users.noreply.github.com>
2024-02-24 15:28:19 +0300

    feat: pagenation

--- Backend/Business/Abstract/IMessageServices.cs
+++ Backend/Business/Abstract/IMessageServices.cs
@@ -14,6 +14,6 @@ public interface IMessageServices
    public long GetIdByConnection(WebSocket ws);
    public Task SendToUser(long senderId, string receiverUserName );
    public Task<IActionResult> SendMessage(string receiverUserName, string message, string token);
-   public Task<IActionResult> GetMessages(string receiverUserName, string token);
+   public Task<IActionResult> GetMessages(string receiverUserName, string token, long page);
    public Task<IActionResult> GetAllLastMessagesGroupByUsers(string token);
 }
