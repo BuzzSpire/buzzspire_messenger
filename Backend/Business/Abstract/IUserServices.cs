d8869ad8eb6c2d7d6020d39f7ea9d207d79d90d5
tw4 <88425310+tw4@users.noreply.github.com>
2024-02-29 10:52:06 +0300

    feat: user update basic info & password

--- Backend/Business/Abstract/IUserServices.cs
+++ Backend/Business/Abstract/IUserServices.cs
@@ -1,4 +1,5 @@
 using Backend.Entity.Concrete;
+using Backend.Entity.DTO.User;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Backend.Business.Abstract;
@@ -7,4 +8,6 @@ public interface IUserServices
 {
    public Task<IActionResult> GetUserByUserNameAsync(string userName, string token);
    Task<IActionResult> UploadProfilePictureAsync(IFormFile file, string token);
+   Task<IActionResult> UpdateUserBasicInfoAsync(UpdateUserBasicInfoRequest request, string token);
+   Task<IActionResult> UpdateUserPasswordAsync(UpdateUserPasswordRequest request, string token);
 }
