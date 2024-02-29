ef06e7d97215f0348808e0156272071ff88b80bd
tw4 <88425310+tw4@users.noreply.github.com>
2024-02-29 12:14:34 +0300

    fix: user register error

--- Backend/Data/Concrete/EF/ApplicationDbContext.cs
+++ Backend/Data/Concrete/EF/ApplicationDbContext.cs
@@ -9,12 +9,13 @@ public class ApplicationDbContext : DbContext
 {
     public DbSet<User> Users { get; set; }
     public DbSet<Message> Messages { get; set; }
-    
+
     private IConfiguration _configuration;
     private IJwtServices _jwtServices;
     private IEncryptServices _encryptServices;
 
-    public ApplicationDbContext(DbContextOptions options, IJwtServices jwtServices, IEncryptServices encryptServices) : base(options)
+    public ApplicationDbContext(DbContextOptions options, IJwtServices jwtServices, IEncryptServices encryptServices) :
+        base(options)
     {
         _jwtServices = jwtServices;
         _encryptServices = encryptServices;
@@ -32,24 +33,5 @@ public class ApplicationDbContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.Entity<User>().HasData(new User
-            {
-                Id = 1,
-                UserName = "admin",
-                Password = _encryptServices.EncryptPassword("admin"),
-                Bio = "admin",
-                FullName = "admin admin",
-                ProfilePicture = new Byte[0]
-            },
-            new User
-            {
-                Id = 2,
-                UserName = "user",
-                Password = _encryptServices.EncryptPassword("user"),
-                Bio = "user",
-                FullName = "user user",
-                ProfilePicture = new Byte[0]
-            }
-        );
     }
 }
